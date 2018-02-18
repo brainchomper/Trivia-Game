1175ab13222b6e762c7473081db8141075ff93db
Kevin Smith <kevin@kvnsmith.com>
2018-02-18 03:37:53 -0700

    working files and bg sound

---
+++ assets/js/game.js
@@ -0,0 +1,23 @@
+// What is Nibler's Poo Made Out of:  Dark Matter
+// What is the Shipp Zapp Brannigan captains? The Nimbus 
+// Who did Amy marry? A:Kif, Zog, Hypno Toad
+// Where is Amy From? Mars, Brooklin, China, Centari B
+// What Food Did Fry Deliver? Pizza, tacos, chinese, 
+// When did fry get cryogenically frozen? 2000: 2210, 1998, 2006
+// Who Was the creator of Futurama? Matt Groening
+// What Species is Zoidberg? Decapodian : Amazonian, Nibblonian, Brain Slug
+// What century is Futurama set in? 31st century: 24th century, 21st century, 40th century
+// What Species has hyp
+
+//sounds
+
+//play background song onload
+$(document).ready(function() {
+	$("#my_audio").get(0).play();
+});
+//end background song
+// function pausePlay(){
+	
+// }
+//end sounds
+
