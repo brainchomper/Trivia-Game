533822c407e7e7aa6481e3b39488566e0e6faa52
Kevin Smith <kevin@kvnsmith.com>
2018-02-19 00:30:14 -0700

    working files

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,46 +2,179 @@ var questionList = [{
 	question: " When did fry get cryogenically frozen?",
 	answerList: ["2210", "1999", "2000", "2006"],
 	answer: 1
-},{
+}, {
 	question: "What Species is Zoidberg?",
 	answerList: ["Amazonian", "Nibblonian", "Brain Slug", "Decapodian"],
 	answer: 3
-},{
-	question: "What Food Did Fry Deliver?",
-	answerList: ["Italian", "Chinese", "Pizza", "Tacos"],
+}, {
+	question: "What Food Did Fry Deliver in the 20th century?",
+	answerList: ["Coffee", "Chinese", "Pizza", "Tacos"],
 	answer: 2
-},{
+}, {
 	question: "Where is Amy From?",
 	answerList: ["Brooklin", "Mars", "China", "Centari B"],
 	answer: 1
-},{
+}, {
 	question: "Who did Amy marry?",
 	answerList: ["Kif Kroker", "Hypno Toad", "Zog", "Morbo"],
 	answer: 0
-},{
+}, {
 	question: "What is the Ship Zapp Brannigan captains?",
 	answerList: ["Planet Express Ship", "Land Titanic", "Kitten Class Attack Ship", "The Nimbus"],
 	answer: 3
-},{	
+}, {
 	question: "What is Nibler's Poo Made Out of?",
 	answerList: ["Gold", "Dark Matter", "Uranium", "Antimatter"],
 	answer: 1
-},{	
+}, {
 	question: "What century is Futurama set in?",
-	answerList: ["21st Century", "24th Century", "31st Century", "40th Century"],
+	answerList: ["21st Century", "24th Century", "30th Century", "40th Century"],
 	answer: 2
-},{	
+}, {
 	question: "Who Was the creator of Futurama?",
 	answerList: ["Chris Sanders", "Matt Groening", "Dan Harmon", "Walt Disney"],
 	answer: 1
-},{	
+}, {
 	question: "What is the name of the Planet Express's Janitor?",
 	answerList: ["Hermes", "Cubert", "Agnew", "Scruffy"],
 	answer: 3
 }];
+var messages = {
+	correct: "<h2>You Got It!</h2>",
+	incorrect: "<h2>Wrong, Be Better!</h2>",
+	noTime: "<h2>You Ran Out Of Time!</h2>",
+	finished: "<h2>You're Done, Let's See How You Did!</h2>"
+}
+var imgArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var currentQuestion;
+var wrongAnswer;
+var incorrectAnswer;
+var unanswered;
+var seconds;
+var time;
+var answered;
+var selected;
 
 //play background song onload
-$(document).ready(function() {
+$(document).ready(function () {
 	$("#my_audio").get(0).play();
 });
 //end background song
+
+//start game btn
+$('#startBtn').on('click', function () {
+	$(this).hide();
+	$('.title').hide();
+	newGame();
+});
+$('#correctImage').on("click", function(){
+	seconds=0;
+	newQuestion()
+	});
+//reset btn
+$('#restartBtn').on('click', function () {
+	$(this).hide();
+	newGame();
+});
+//start - new game
+function newGame() {
+	$('#finalMessage').empty();
+	$('#correctCount').empty();
+	$('#wrongCount').empty();
+	$('#notAnswered').empty();
+	currentQuestion = 0;
+	correctAnswer = 0;
+	wrongAnswer = 0;
+	unanswered = 0;
+	newQuestion();
+}
+
+//start new questions
+function newQuestion() {
+	$('#message').empty();
+	$('#correctAnswer').empty();
+	$('#correctImage').empty();
+	$('.listOfAnswers').empty();
+	answered = true;
+
+	$('#currentQuestion').html('Question #' + (currentQuestion + 1) + '/' + questionList.length);
+	$('.question').html('<h2>' + questionList[currentQuestion].question + '</h2>');
+	for (var i = 0; i < 4; i++) {
+		var choices = $('<div>');
+		choices.text(questionList[currentQuestion].answerList[i]);
+		choices.attr({ 'data-index': i });
+		choices.addClass('thisChoice');
+		$('.listOfAnswers').append(choices);
+	}
+	countdown();
+	//clicking an answer will pause the time and setup answerPage
+	$('.thisChoice').on('click', function () {
+		selected = $(this).data('index');
+		clearInterval(time);
+		answerPage();
+	});
+}
+
+function countdown() {
+	seconds = 15;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	answered = true;
+	//sets timer to go down
+	time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown() {
+	seconds--;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	if (seconds < 1) {
+		clearInterval(time);
+		answered = false;
+		answerPage();
+	}
+}
+
+function answerPage() {
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
+	$('.listOfAnswers').empty();
+
+	var rightAnswerText = questionList[currentQuestion].answerList[questionList[currentQuestion].answer];
+	var rightAnswerIndex = questionList[currentQuestion].answer;
+	$('#correctImage').html('<img src = "assets/images/' + imgArray[currentQuestion] + '.gif" width = "350px" class="image">');
+	//checks to see correct, incorrect, or unanswered
+	if ((selected == rightAnswerIndex) && (answered == true)) {
+		correctAnswer++;
+		$('#message').html(messages.correct);
+	} else if ((selected != rightAnswerIndex) && (answered == true)) {
+		incorrectAnswer++;
+		$('#message').html(messages.incorrect);
+		$('#correctAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else {
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+
+	if (currentQuestion == (questionList.length - 1)) {
+		setTimeout(scoreboard, 3000)
+	} else {
+		currentQuestion++;
+		setTimeout(newQuestion, 3000);
+	}
+}
+function scoreboard() {
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctAnswer').empty();
+	$('#correctImage').empty();
+
+	$('#finalMessage').html(messages.finished);
+	$('#correctCount').html("Correct Answers: " + correctAnswer);
+	$('#wrongCount').html("Incorrect Answers: " + incorrectAnswer);
+	$('#notAnswered').html("Unanswered: " + unanswered);
+	$('#restartBtn').addClass('reset');
+	$('#restartBtn').show();
+	$('#restartBtn').html('Start Over?');
+}
