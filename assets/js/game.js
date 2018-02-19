92ec686588e9245b3fbca5bf4fb6cb0f9fdbb0d4
Kevin Smith <kevin@kvnsmith.com>
2018-02-18 20:08:44 -0700

    trivia questions and answers

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,47 @@
-// What is Nibler's Poo Made Out of:  Dark Matter
-// What is the Shipp Zapp Brannigan captains? The Nimbus 
-// Who did Amy marry? A:Kif, Zog, Hypno Toad
-// Where is Amy From? Mars, Brooklin, China, Centari B
-// What Food Did Fry Deliver? Pizza, tacos, chinese, 
-// When did fry get cryogenically frozen? 2000: 2210, 1998, 2006
-// Who Was the creator of Futurama? Matt Groening
-// What Species is Zoidberg? Decapodian : Amazonian, Nibblonian, Brain Slug
-// What century is Futurama set in? 31st century: 24th century, 21st century, 40th century
-// What Species has hyp
-
-//sounds
+var questionList = [{
+	question: " When did fry get cryogenically frozen?",
+	answerList: ["2210", "1999", "2000", "2006"],
+	answer: 1
+},{
+	question: "What Species is Zoidberg?",
+	answerList: ["Amazonian", "Nibblonian", "Brain Slug", "Decapodian"],
+	answer: 3
+},{
+	question: "What Food Did Fry Deliver?",
+	answerList: ["Italian", "Chinese", "Pizza", "Tacos"],
+	answer: 2
+},{
+	question: "Where is Amy From?",
+	answerList: ["Brooklin", "Mars", "China", "Centari B"],
+	answer: 1
+},{
+	question: "Who did Amy marry?",
+	answerList: ["Kif Kroker", "Hypno Toad", "Zog", "Morbo"],
+	answer: 0
+},{
+	question: "What is the Ship Zapp Brannigan captains?",
+	answerList: ["Planet Express Ship", "Land Titanic", "Kitten Class Attack Ship", "The Nimbus"],
+	answer: 3
+},{	
+	question: "What is Nibler's Poo Made Out of?",
+	answerList: ["Gold", "Dark Matter", "Uranium", "Antimatter"],
+	answer: 1
+},{	
+	question: "What century is Futurama set in?",
+	answerList: ["21st Century", "24th Century", "31st Century", "40th Century"],
+	answer: 2
+},{	
+	question: "Who Was the creator of Futurama?",
+	answerList: ["Chris Sanders", "Matt Groening", "Dan Harmon", "Walt Disney"],
+	answer: 1
+},{	
+	question: "What is the name of the Planet Express's Janitor?",
+	answerList: ["Hermes", "Cubert", "Agnew", "Scruffy"],
+	answer: 3
+}];
 
 //play background song onload
 $(document).ready(function() {
 	$("#my_audio").get(0).play();
 });
 //end background song
-// function pausePlay(){
-	
-// }
-//end sounds
-
